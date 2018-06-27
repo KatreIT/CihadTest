fb05692a623b03e90749f23a4820887285227495
m.alkan <muratalkan66@outlook.com>
2018-06-27 23:05:38 +0300

    version 1.1

--- CihadTest/Program.cs
+++ CihadTest/Program.cs
@@ -10,7 +10,7 @@ namespace CihadTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("deneme");
         }
     }
 }
